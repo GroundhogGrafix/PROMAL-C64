850c9c4a80ddda15bc096984dc3760a1ec2a065e
Martin Sigley <firebreathinggroundhog@yahoo.com>
2022-06-22 16:05:29 -0500

    Initial commit

---
+++ Doodling/PROLAUNCH.bat
@@ -0,0 +1,6 @@
+cd\Users\Someone\Documents\Commodore\1541 Ultimate II\Programming\Promal\Work Folder
+
+c1541.exe -attach %1.d64 -write %2.s @8:%2.s,seq
+xscpu64.exe -8 %1.d64 -autostart "C:\Users\Someone\Documents\WinVICE-2.4-x86 (scpu64)\SCPU64\quicksnap9.vsf"
+
+
